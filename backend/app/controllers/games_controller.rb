a83d40182d14c6562649697ec610d5376cd4e06f
talryan <nataliamryan@gmail.com>
2021-02-17 20:59:28 -0800

    Controllers Added.

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -1,51 +1,28 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: [:show, :update, :destroy]
 
-  # GET /games
   def index
-    @games = Game.all
-
-    render json: @games
+      @games = Game.all
+      render :json => @games, :include => {:user => {:only => :name}}
   end
 
-  # GET /games/1
-  def show
-    render json: @game
-  end
-
-  # POST /games
   def create
-    @game = Game.new(game_params)
-
-    if @game.save
-      render json: @game, status: :created, location: @game
-    else
-      render json: @game.errors, status: :unprocessable_entity
-    end
+      @game = Game.new(game_params)
+      if @game.save
+          render :json => @game, :include => {:user => {:only => :name}}
+      else
+          render :json => { :errors => @game.errors }, status: 400
+      end
   end
 
-  # PATCH/PUT /games/1
-  def update
-    if @game.update(game_params)
-      render json: @game
-    else
-      render json: @game.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /games/1
-  def destroy
-    @game.destroy
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_game
-      @game = Game.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def game_params
+  def game_params
       params.require(:game).permit(:user_id, :score)
-    end
+  end
+
+  def find_game
+      game = Game.find_by(id: params[:id])
+      game
+  end
 end
